e554c66a93caf2a5316d6e0e2ce3d33306073a0d
hahuyhoang411 <hahuyhoanghhh41@gmail.com>
2025-03-17 00:58:56 +0100

    update

--- training/grpo.py
+++ training/grpo.py
@@ -17,7 +17,7 @@ model, tokenizer = FastLanguageModel.from_pretrained(
     load_in_4bit = True, # False for LoRA 16bit
     fast_inference = True, # Enable vLLM fast inference
     max_lora_rank = lora_rank,
-    gpu_memory_utilization = 0.4 # Reduce if out of memory
+    gpu_memory_utilization = 0.3 # Reduce if out of memory
 )
 
 model = FastLanguageModel.get_peft_model(
